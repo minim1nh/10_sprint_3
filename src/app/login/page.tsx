cfb06e4f43d615b8565202a790fe5cd85967c76b
April-37 <150885908+April-37@users.noreply.github.com>
2024-11-25 09:17:49 +0900

    feat : 로그인 페이지 기본 구조 작성

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,21 @@
+import style from "@/styles/login/style.module.scss";
+
 export default function Login() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className={style.containerLogin}>
+      <div className={style.containerHeader}>로그인</div>
+        <form className={style.containerForm}>
+          <label className={style.containerLabel} htmlFor="email">
+            이메일<br />
+            <input className={style.containerInput} name="email" type="text" placeholder="이메일을 입력해 주세요" />
+          </label><br />
+          <label className={style.containerLabel} htmlFor="pwd">
+            비밀번호<br />
+            <input className={style.containerInput} name="pwd" type="password" placeholder="비밀번호를 입력해 주세요" />
+          </label><br />
+          <input className={style.btnLogin} type="button" value="로그인" />
+        </form><br /><br />
+        <div className={style.containerSignup}><a href="/signup">회원가입</a></div>
     </div>
   );
 }
