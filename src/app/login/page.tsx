7d669771837fe101103e906b51efcfa84a1f2c08
April-37 <150885908+April-37@users.noreply.github.com>
2024-11-27 13:01:46 +0900

    feat : invalid input error handle 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,21 +8,34 @@ interface FormTypes {
   pwd: string;
 }
 export default function Login() {
-  const { register, handleSubmit } = useForm<FormTypes>();
+  const { register, handleSubmit, formState: { errors }, } = useForm<FormTypes>({ mode: "onBlur"});
   const onValid = (data: FormTypes) => console.log(data);
+
   return (
     <div className={style.containerLogin}>
       <div className={style.containerHeader}>로그인</div>
         <form className={style.containerForm} onSubmit={handleSubmit(onValid)}>
           <label className={style.containerLabel} htmlFor="email">
             이메일<br />
-            <input className={style.containerInput} {...register("email", {required: true, minLength: 8})}
-             name="email" type="text" placeholder="이메일을 입력해 주세요" />
+            <input className={`${style.containerInput} ${errors.email ? style.errorInput : ""}`} 
+            {...register("email", { required: "이메일 형식으로 작성해 주세요.", 
+              pattern: {
+                value: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,4}$/i,
+                message: "이메일 형식으로 작성해 주세요.",
+              },})}
+             id="email" name="email" type="text" placeholder="이메일을 입력해 주세요" />
+             { errors.email && <small className={style.tagAlert} role="alert">{errors.email.message}</small>}
           </label><br />
           <label className={style.containerLabel} htmlFor="pwd">
             비밀번호<br />
-            <input className={style.containerInput} {...register("pwd", {required: true, minLength: 8})}
-            name="pwd" type="password" placeholder="비밀번호를 입력해 주세요" />
+            <input className={`${style.containerInput} ${errors.pwd ? style.errorInput : ""}`} 
+            {...register("pwd", {required: "8자 이상 입력해 주세요.", 
+            minLength: {
+              value: 8,
+              message: "8자 이상 입력해 주세요.",
+            },})}
+            id="pwd" name="pwd" type="password" placeholder="비밀번호를 입력해 주세요" />
+            { errors.pwd && <small className={style.tagAlert} role="alert">{errors.pwd.message}</small> }
           </label><br />
           <input className={style.btnLogin} type="submit" value="로그인" />
         </form><br /><br />
