978eb644ba12ca35c1b0c00372ee515f9558543e
한수진 <mini@MINIM.local>
2024-11-28 11:55:39 +0900

    fix: 상세게시판 댓글 컴포넌트 수정

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,17 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
   images: {
-    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
