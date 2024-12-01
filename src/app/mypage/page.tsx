e6bb4d17e5ee327af89eb3300f9d930317553863
April-37 <150885908+April-37@users.noreply.github.com>
2024-12-01 13:57:23 +0900

    feat : 위키 페이지 생성 기능 구현

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -2,11 +2,25 @@
 
 import style from "@/styles/mypage/style.module.scss";
 import { useForm } from "react-hook-form";
+import { useState, useEffect } from "react";
+import { redirect } from "next/navigation";
 import { getUsersMe, patchUsersMePassword } from '@/api/swagger/User';
-import { PasswordProps } from '@/api/swagger/Wikid.types';
+import { PasswordProps, ProfilesProps } from '@/api/swagger/Wikid.types';
+import { postProfiles } from "@/api/swagger/Profile";
+
+type passFormInput = PasswordProps;
+type wikiFormInput = ProfilesProps;
 
 export default function MyPage() {
-  const { register, getValues, handleSubmit, formState: { errors }, } = useForm<PasswordProps>({ mode: "onBlur"});
+  const [isWikiExist, setIsWikiExist] = useState(false);
+
+  const passwordForm = useForm<passFormInput>({ mode: "onBlur"});
+  const wikiForm = useForm<wikiFormInput>({ mode: "onBlur"});
+
+  useEffect(() => {
+    if(isWikiExist)
+      redirect('/wiki');
+  }, [isWikiExist]);
 
   const onValid = async (data: PasswordProps) => {
     const reqData = {
@@ -24,48 +38,80 @@ export default function MyPage() {
     } 
   }
 
+  const checkWikiExist = async (data: ProfilesProps) => {
+    try {
+      const resData = await getUsersMe();
+      if(resData !== null) {
+        if(resData.profile != null) {
+          setIsWikiExist(true);
+          alert('이미 위키 페이지가 존재합니다!');
+        }
+        else {
+          const reqProf = {
+            securityQuestion: data.securityQuestion,
+            securityAnswer: data.securityAnswer,
+          }
+          const resProf = await postProfiles(reqProf);
+          console.log(JSON.stringify(resProf));
+        }
+      }
+    }
+    catch(e) {
+      console.log(e);
+      console.log('위키 페이지 생성에 실패했습니다.');
+    }
+  }
+
   return (
     <div className={style.containerMypage}>
       <div className={style.containerHeader}>계정 설정</div>
-      <form className={style.containerForm} onSubmit={handleSubmit(onValid)}>
+      <form className={style.containerForm} onSubmit={passwordForm.handleSubmit(onValid)}>
         <label className={style.containerLabel}>
           비밀번호 변경<br />
-          <input className={`${style.containerInput} ${errors.currentPassword ? style.errorInput : ""}`}
-           {...register("currentPassword", {required: "기존 비밀번호를 입력해 주세요.", 
+          <input className={`${style.containerInput} ${passwordForm.formState.errors.currentPassword ? style.errorInput : ""}`}
+           {...passwordForm.register("currentPassword", {required: "기존 비밀번호를 입력해 주세요.", 
             minLength: {
               value: 8,
               message: "8자 이상 입력해 주세요.",
             },})} id="currentPassword" name="currentPassword" type="password" placeholder="기존 비밀번호" />
-            { errors.currentPassword && <small className={style.tagAlert} role="alert">{errors.currentPassword.message}</small> }
+            { passwordForm.formState.errors.currentPassword && <small className={style.tagAlert} role="alert">{passwordForm.formState.errors.currentPassword.message}</small> }
             
-          <input className={`${style.containerInput} ${errors.password ? style.errorInput : ""}`}
-             {...register("password", {required: "8자 이상 입력해 주세요.", 
+          <input className={`${style.containerInput} ${passwordForm.formState.errors.password ? style.errorInput : ""}`}
+             {...passwordForm.register("password", {required: "8자 이상 입력해 주세요.", 
             minLength: {
               value: 8,
               message: "8자 이상 입력해 주세요.",
             },})} id="password" name="password" type="password" placeholder="새 비밀번호" />
-            { errors.password && <small className={style.tagAlert} role="alert">{errors.password.message}</small> }
+            { passwordForm.formState.errors.password && <small className={style.tagAlert} role="alert">{passwordForm.formState.errors.password.message}</small> }
           
-          <input className={`${style.containerInput} ${errors.passwordConfirmation ? style.errorInput : ""}`} 
-          {...register("passwordConfirmation", {required: "비밀번호가 일치하지 않습니다.", 
+          <input className={`${style.containerInput} ${passwordForm.formState.errors.passwordConfirmation ? style.errorInput : ""}`} 
+          {...passwordForm.register("passwordConfirmation", {required: "비밀번호가 일치하지 않습니다.", 
             validate: {
               check: (val) => {
-                if(getValues("password") !== val)
+                if(passwordForm.getValues("password") !== val)
                   return "비밀번호가 일치하지 않습니다.";
               }
             },})} id="passwordConfirmation" name="passwordConfirmation" type="password" placeholder="새 비밀번호 확인" />
-            { errors.passwordConfirmation && <small className={style.tagAlert} role="alert">{errors.passwordConfirmation.message}</small> }
+            { passwordForm.formState.errors.passwordConfirmation && <small className={style.tagAlert} role="alert">{passwordForm.formState.errors.passwordConfirmation.message}</small> }
         </label>
         <input className={style.btn} type="submit" value="변경하기" />
       </form>
+
       <br /><hr className={style.styleHr} /><br />
-      <form className={style.containerForm}>
+
+      <form className={style.containerForm} onSubmit={wikiForm.handleSubmit(checkWikiExist)}>
         <label className={style.containerLabel}>
           위키 생성하기<br />
-          <input className={style.containerInput} name="wikiQues" type="text" placeholder="질문을 입력해 주세요" />
-          <input className={style.containerInput} name="wikiAns" type="text" placeholder="답을 입력해 주세요" />
+          <input className={`${style.containerInput} ${wikiForm.formState.errors.securityQuestion ? style.errorInput : ""}`}
+          {...wikiForm.register("securityQuestion", {required: "질문을 입력해 주세요.", })}
+           id="securityQuestion" name="securityQuestion" type="text" placeholder="질문을 입력해 주세요" />
+          { wikiForm.formState.errors.securityQuestion && <small className={style.tagAlert} role="alert">{wikiForm.formState.errors.securityQuestion.message}</small> }
+          <input className={`${style.containerInput} ${wikiForm.formState.errors.securityAnswer ? style.errorInput : ""}`}
+          {...wikiForm.register("securityAnswer", {required: "답을 입력해 주세요.", })}
+           id="securityAnswer" name="securityAnswer" type="text" placeholder="답을 입력해 주세요" />
+          { wikiForm.formState.errors.securityAnswer && <small className={style.tagAlert} role="alert">{wikiForm.formState.errors.securityAnswer.message}</small> }
         </label>
-        <input className={style.btn} type="button" value="생성하기" />
+        <input className={style.btn} type="submit" value="생성하기" />
       </form>
     </div>
   );
